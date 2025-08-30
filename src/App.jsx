92c0ef79f3a077196263fc84492e1f90319b298d
Abdullah-Arif-5065 <abdullaharif5065@gmail.com>
2025-08-30 23:45:24 +0530

    Cleaned nul issue and updated project files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Counter from './components/Counter'
 import Todos from './components/todo_list/Todos'
+import Meals from './components/todo_list/Meals'
 
 const App = () => {
   return (
     <div>
       {/* < Counter /> */}
-      <Todos />
+      {/* <Todos /> */}
+      < Meals />
     </div>
   )
 }
